7b185a0008887bcffc7eee63ba05dda6e70c6c5c
jccabarlo <jessecabarlo@gmail.com>
2025-07-15 14:29:24 +0800

    Refactor App composable to utilize MainViewModel for greeting list and remove unused components

--- composeApp/src/androidMain/kotlin/com/example/kmp/App.kt
+++ composeApp/src/androidMain/kotlin/com/example/kmp/App.kt
@@ -1,43 +1,37 @@
 package com.example.kmp
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import kmp.composeapp.generated.resources.Res
-import kmp.composeapp.generated.resources.compose_multiplatform
 
 @Composable
 @Preview
-fun App() {
+fun App(mainViewModel: MainViewModel = viewModel()) {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
+        val greetings by mainViewModel.greetingList.collectAsStateWithLifecycle()
+
         Column(
             modifier = Modifier
+                .padding(all = 10.dp)
                 .safeContentPadding()
                 .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(8.dp),
         ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
+            greetings.forEach { greeting ->
+                Text(greeting)
+                HorizontalDivider()
             }
         }
     }
