44017b40db60d4b93f24c8d3b7f10441efc5aa50
jccabarlo <jessecabarlo@gmail.com>
2025-07-17 15:52:28 +0800

    Refactor Greeting.kt to remove platform-specific greeting
    The platform-specific greeting "Guess what this is! > ${platform.name.reversed()}" has been commented out.
    This change simplifies the greeting logic and removes the dependency on the `getPlatform()` function.

--- shared/src/commonMain/kotlin/com/example/kmp/Greeting.kt
+++ shared/src/commonMain/kotlin/com/example/kmp/Greeting.kt
@@ -7,13 +7,12 @@ import kotlin.random.Random
 import kotlin.time.Duration.Companion.seconds
 
 class Greeting {
-    private val platform = getPlatform()
     private val rocketComponent = RocketComponent()
 
     fun greet(): Flow<String> = flow {
         emit(if (Random.nextBoolean()) "Hi!" else "Hello!")
         delay(1.seconds)
-        emit("Guess what this is! > ${platform.name.reversed()}")
+//        emit("Guess what this is! > ${platform.osName}")
         delay(1.seconds)
         emit(daysPhrase())
         emit(rocketComponent.launchPhrase())
