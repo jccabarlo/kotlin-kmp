77f90ae1cf1f94d65ecf659743a0f393508d73c5
jccabarlo <jessecabarlo@gmail.com>
2025-07-17 15:52:08 +0800

    Refactor IOSPlatform class to include OS name, OS version, device model, device density, and logging functionality

--- shared/src/iosMain/kotlin/com/example/kmp/Platform.ios.kt
+++ shared/src/iosMain/kotlin/com/example/kmp/Platform.ios.kt
@@ -1,9 +1,26 @@
 package com.example.kmp
 
+import platform.Foundation.NSLog
 import platform.UIKit.UIDevice
+import platform.UIKit.UIScreen
 
-class IOSPlatform: Platform {
-    override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
+//class IOSPlatform: Platform {
+//    override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
+//}
+
+actual class Platform {
+    actual val osName: String
+        get() = UIDevice.currentDevice.systemName
+    actual val osVersion: String
+        get() = UIDevice.currentDevice.systemVersion()
+    actual val deviceModel: String
+        get() = UIDevice.currentDevice.model
+    actual val deviceDensity: Int
+        get() = UIScreen.mainScreen.scale.toInt()
+
+    actual fun logSystemInfo() {
+        NSLog(
+            "($osName $osVersion $deviceModel $deviceDensity)"
+        )
+    }
 }
-
-actual fun getPlatform(): Platform = IOSPlatform()
