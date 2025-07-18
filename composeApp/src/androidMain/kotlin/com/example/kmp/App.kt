5102886a27650d2eb3434b7feee46060aee19612
jccabarlo <jessecabarlo@gmail.com>
2025-07-18 14:02:20 +0800

    Fetch and display product names in a LazyColumn
    This commit modifies the `App` composable to:
    - Collect product data from `MainViewModel`.
    - Display the product names in a `LazyColumn`.
    - Each product name is separated by a `HorizontalDivider`.

--- composeApp/src/androidMain/kotlin/com/example/kmp/App.kt
+++ composeApp/src/androidMain/kotlin/com/example/kmp/App.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -21,6 +23,7 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 fun App(mainViewModel: MainViewModel = viewModel()) {
     MaterialTheme {
         val greetings by mainViewModel.greetingList.collectAsStateWithLifecycle()
+        val products by mainViewModel.productList.collectAsStateWithLifecycle()
         Platform().logSystemInfo()
         Column(
             modifier = Modifier
@@ -32,6 +35,18 @@ fun App(mainViewModel: MainViewModel = viewModel()) {
             greetings.forEach { greeting ->
                 Text(greeting)
                 HorizontalDivider()
+            }
+
+            LazyColumn {
+                items(products) { it ->
+                    Column(
+                        modifier = Modifier
+                            .padding(horizontal = 10.dp, vertical = 20.dp),
+                    ) {
+                        Text(text = it.name)
+                    }
+                    HorizontalDivider()
+                }
             }
         }
     }
