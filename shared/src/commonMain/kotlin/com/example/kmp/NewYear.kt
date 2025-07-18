edbaee081deefd222c68116b045efe587227c2f6
jccabarlo <jessecabarlo@gmail.com>
2025-07-18 13:07:49 +0800

    refactor(NewYear.kt) import from kotlinx.datetime and replace deprecated Clock

--- shared/src/commonMain/kotlin/com/example/kmp/NewYear.kt
+++ shared/src/commonMain/kotlin/com/example/kmp/NewYear.kt
@@ -1,9 +1,15 @@
 package com.example.kmp
 
-import kotlinx.datetime.*
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.daysUntil
+import kotlinx.datetime.todayIn
+import kotlin.time.Clock
+import kotlin.time.ExperimentalTime
 
+@OptIn(ExperimentalTime::class)
 fun daysUntilNewYear(): Int {
-    val today = Clock.System.todayIn(TimeZone.currentSystemDefault())
+    val today = Clock.System.todayIn (TimeZone.currentSystemDefault())
     val closestNewYear = LocalDate(today.year + 1, 1, 1)
     return today.daysUntil(closestNewYear)
 }
