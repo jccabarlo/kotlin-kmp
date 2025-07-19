2db16fa03c5f0bc9bec371e557faee0845e18460
jccabarlo <jessecabarlo@gmail.com>
2025-07-19 19:21:14 +0800

    Add device information display to Android app
    This commit introduces functionality to display device-specific information within the Android application.
    
    Key changes:
    - Created a `DeviceInfo` Composable function to display a list of device properties.
    - Implemented `makeItems` function to gather device information like OS name, OS version, device model, and density using the `Platform` class.
    - Added a `RowView` Composable to display each key-value pair of device information.
    - Utilized `LazyColumn` for efficient display of the device information list.
    - Integrated `HorizontalDivider` to visually separate items in the list.
    - Updated the `Content` Composable to potentially include `DeviceInfo`.
    - Minor UI adjustments in `Content` for `fillMaxSize` and padding.

--- composeApp/src/androidMain/kotlin/com/example/kmp/App.kt
+++ composeApp/src/androidMain/kotlin/com/example/kmp/App.kt
@@ -2,10 +2,12 @@ package com.example.kmp
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.DividerDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
@@ -15,6 +17,7 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -23,12 +26,10 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 @Composable
 @Preview
 fun App(mainViewModel: MainViewModel = viewModel()) {
-    Platform().logSystemInfo()
     MaterialTheme {
         Scaffold(topBar = { ToolBar() }) { paddingValues ->
             Content(
-                mainViewModel,
-                paddingValues
+                mainViewModel, paddingValues
             )
         }
 
@@ -39,8 +40,7 @@ fun App(mainViewModel: MainViewModel = viewModel()) {
 @Composable
 private fun ToolBar() {
     TopAppBar(
-        title = { Text(text = "Home") }
-    )
+        title = { Text(text = "Home") })
 }
 
 @Composable
@@ -49,6 +49,7 @@ private fun Content(viewModel: MainViewModel, paddingValues: PaddingValues) {
     val products by viewModel.productList.collectAsStateWithLifecycle()
     Column(
         modifier = Modifier
+            .fillMaxSize()
             .padding(paddingValues)
             .padding(horizontal = 16.dp)
     ) {
@@ -59,11 +60,12 @@ private fun Content(viewModel: MainViewModel, paddingValues: PaddingValues) {
             }
         }
 
+//        DeviceInfo()
+
         LazyColumn {
             items(products) { it ->
                 Column(
-                    modifier = Modifier
-                        .padding(horizontal = 10.dp, vertical = 20.dp),
+                    modifier = Modifier.padding(horizontal = 10.dp, vertical = 20.dp),
                 ) {
                     Text(text = it.name)
                 }
@@ -71,4 +73,47 @@ private fun Content(viewModel: MainViewModel, paddingValues: PaddingValues) {
             }
         }
     }
+}
+
+@Composable
+private fun DeviceInfo() {
+    val items = makeItems()
+
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        items(items) { row ->
+            RowView(title = row.first, subtitle = row.second)
+        }
+    }
+}
+
+private fun makeItems(): List<Pair<String, String>> {
+    val platform = Platform()
+    platform.logSystemInfo()
+
+    return listOf(
+        Pair("Operating System", "${platform.osName} ${platform.osVersion}"),
+        Pair("Device", platform.deviceModel),
+        Pair("Density", platform.deviceDensity.toString())
+    )
+}
+
+@Composable
+private fun RowView(
+    title: String,
+    subtitle: String,
+) {
+    Column(Modifier.padding(8.dp)) {
+        Text(
+            text = title,
+            style = MaterialTheme.typography.bodySmall,
+            color = Color.Gray,
+        )
+        Text(
+            text = subtitle,
+            style = MaterialTheme.typography.bodyLarge,
+        )
+    }
+    HorizontalDivider(Modifier, DividerDefaults.Thickness, DividerDefaults.color)
 }
