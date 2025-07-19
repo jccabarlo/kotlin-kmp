eafef47c2a85194482e5a94bacf57b6eed3dba17
jccabarlo <jessecabarlo@gmail.com>
2025-07-19 16:27:42 +0800

    Refactor App.kt to use Scaffold with TopAppBar and Content
    The `App` composable function in `composeApp/src/androidMain/kotlin/com/example/kmp/App.kt` has been refactored to use a `Scaffold`.
    
    This change introduces:
    - A `ToolBar` composable for the top app bar.
    - A `Content` composable to display the main content, including greetings and a list of products.
    - Appropriate padding is applied using `PaddingValues` from the `Scaffold`.

--- composeApp/src/androidMain/kotlin/com/example/kmp/App.kt
+++ composeApp/src/androidMain/kotlin/com/example/kmp/App.kt
@@ -1,15 +1,17 @@
 package com.example.kmp
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
@@ -21,32 +23,51 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 @Composable
 @Preview
 fun App(mainViewModel: MainViewModel = viewModel()) {
+    Platform().logSystemInfo()
     MaterialTheme {
-        val greetings by mainViewModel.greetingList.collectAsStateWithLifecycle()
-        val products by mainViewModel.productList.collectAsStateWithLifecycle()
-        Platform().logSystemInfo()
-        Column(
-            modifier = Modifier
-                .padding(all = 10.dp)
-                .safeContentPadding()
-                .fillMaxSize(),
-            verticalArrangement = Arrangement.spacedBy(8.dp),
-        ) {
-            greetings.forEach { greeting ->
+        Scaffold(topBar = { ToolBar() }) { paddingValues ->
+            Content(
+                mainViewModel,
+                paddingValues
+            )
+        }
+
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun ToolBar() {
+    TopAppBar(
+        title = { Text(text = "Home") }
+    )
+}
+
+@Composable
+private fun Content(viewModel: MainViewModel, paddingValues: PaddingValues) {
+    val greetings by viewModel.greetingList.collectAsStateWithLifecycle()
+    val products by viewModel.productList.collectAsStateWithLifecycle()
+    Column(
+        modifier = Modifier
+            .padding(paddingValues)
+            .padding(horizontal = 16.dp)
+    ) {
+        greetings.forEach { greeting ->
+            Column(modifier = Modifier.height(80.dp)) {
                 Text(greeting)
                 HorizontalDivider()
             }
+        }
 
-            LazyColumn {
-                items(products) { it ->
-                    Column(
-                        modifier = Modifier
-                            .padding(horizontal = 10.dp, vertical = 20.dp),
-                    ) {
-                        Text(text = it.name)
-                    }
-                    HorizontalDivider()
+        LazyColumn {
+            items(products) { it ->
+                Column(
+                    modifier = Modifier
+                        .padding(horizontal = 10.dp, vertical = 20.dp),
+                ) {
+                    Text(text = it.name)
                 }
+                HorizontalDivider()
             }
         }
     }
