f09f6cc7848c91db01796eb7760efcae75289ff8
jccabarlo <jessecabarlo@gmail.com>
2025-07-23 06:02:24 +0800

    Add AppScaffold and AppNavHost Composables for Navigation
    This commit introduces `AppScaffold.kt` which includes:
    - `AppScaffold`: A Composable function that sets up the basic Material Design visual layout structure using `Scaffold`. It initializes a `rememberNavController`.
    - `AppNavHost`: A Composable function responsible for defining the navigation graph using `NavHost`. It sets up routes for `ArticlesScreen` and `DeviceInfoScreen`, enabling navigation between them.
    
    The `AppScaffold` uses `AppNavHost` to manage screen transitions based on user interactions.

---
+++ composeApp/src/androidMain/kotlin/com/example/kmp/AppScaffold.kt
@@ -0,0 +1,56 @@
+package com.example.kmp
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.kmp.articles.ArticlesViewModel
+import com.example.kmp.screens.ArticlesScreen
+import com.example.kmp.screens.DeviceInfoScreen
+import com.example.kmp.screens.Screens
+
+@Composable
+fun AppScaffold(articlesViewModel: ArticlesViewModel) {
+    val navController = rememberNavController()
+
+    Scaffold {
+        AppNavHost(
+            navController = navController,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it),
+            articlesViewModel
+        )
+    }
+}
+
+@Composable
+fun AppNavHost(
+    navController: NavHostController,
+    modifier: Modifier = Modifier,
+    articlesViewModel: ArticlesViewModel
+) {
+    NavHost(
+        navController = navController,
+        startDestination = Screens.ARTICLES.route,
+        modifier = modifier,
+    ) {
+        composable(Screens.ARTICLES.route) {
+            ArticlesScreen(
+                onAboutButtonClick = { navController.navigate(Screens.DEVICE_INFO.route) },
+                articlesViewModel,
+            )
+        }
+
+        composable(Screens.DEVICE_INFO.route) {
+            DeviceInfoScreen(
+                onUpButtonClick = { navController.popBackStack() }
+            )
+        }
+    }
+}
