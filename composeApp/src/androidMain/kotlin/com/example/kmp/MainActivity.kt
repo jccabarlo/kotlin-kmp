50739c9ec5fa21f3d643ea5c3468cedd874318a7
jccabarlo <jessecabarlo@gmail.com>
2025-07-22 15:45:17 +0800

    Integrate ArticlesViewModel into MainActivity
    The ArticlesViewModel is now initialized in MainActivity using `viewModels()` and passed to the `ArticlesScreen`.

--- composeApp/src/androidMain/kotlin/com/example/kmp/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/example/kmp/MainActivity.kt
@@ -4,16 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.kmp.articles.ArticlesViewModel
+import com.example.kmp.screens.ArticlesScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
 
+        val articlesViewModel: ArticlesViewModel by viewModels()
+
         setContent {
-            App()
+            ArticlesScreen(
+                {},
+                articlesViewModel = articlesViewModel
+            )
         }
     }
 }
