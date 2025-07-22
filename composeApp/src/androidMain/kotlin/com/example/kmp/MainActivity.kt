e2675578d0ce1482fdd5edf32f13875d1934ac6e
jccabarlo <jessecabarlo@gmail.com>
2025-07-23 06:02:47 +0800

    Refactor MainActivity to use App composable
    The MainActivity in the `composeApp` module has been updated to directly use the `App()` composable function from the `commonMain` source set.
    
    This change streamlines the Android entry point by removing the previously used `ArticlesScreen` and its associated `ArticlesViewModel`. The preview function `AppAndroidPreview` has also been commented out as it's no longer directly relevant.

--- composeApp/src/androidMain/kotlin/com/example/kmp/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/example/kmp/MainActivity.kt
@@ -4,30 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.kmp.articles.ArticlesViewModel
-import com.example.kmp.screens.ArticlesScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
 
-        val articlesViewModel: ArticlesViewModel by viewModels()
-
+//        val articlesViewModel: ArticlesViewModel by viewModels()
         setContent {
-            ArticlesScreen(
-                {},
-                articlesViewModel = articlesViewModel
-            )
+           App()
         }
     }
 }
 
-@Preview
-@Composable
-fun AppAndroidPreview() {
-    App()
-}
+//@Preview
+//@Composable
+//fun AppAndroidPreview() {
+//    App()
+//}
